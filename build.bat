e07ef4a085b240106630a9ac305a214964d34bf2
vladmihalcea <mih_vlad@yahoo.com>
2015-07-07 15:10:54 +0300

    #24  Add build scripts to automate the release procedure
    Add usage section to build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
+@echo off
+
+if "%1" == "" goto usage
+
 SET passphrase=%1
-mvn -P release Dgpg.passphrase=%passphrase% -DignoreSnapshots=true release:clean release:prepare
-mvn -P release Dgpg.passphrase=%passphrase% release:perform
+mvn -P release -Dgpg.passphrase=%passphrase% -DignoreSnapshots=true release:clean release:prepare
+mvn -P release -Dgpg.passphrase=%passphrase% release:perform
+goto:eof
+
+:usage
+echo Usage: %0 passphrase
