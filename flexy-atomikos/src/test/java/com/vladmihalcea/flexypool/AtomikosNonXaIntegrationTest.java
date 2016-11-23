a12f88bbc65609d9ddcd60aeac907f02d300376b
Andreas Brenk <mail@andreasbrenk.com>
2016-11-23 20:24:00 +0100

    Add support for Atomikos using non-XA JDBC implementations #38

---
+++ flexy-atomikos/src/test/java/com/vladmihalcea/flexypool/AtomikosNonXaIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.vladmihalcea.flexypool;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * AtomikosIntegrationTest - Atomikos Integration Test
+ *
+ * @author Vlad Mihalcea
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/applicationContext-non-xa-test.xml")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class AtomikosNonXaIntegrationTest extends AbstractPoolAdapterIntegrationTest {
+}
