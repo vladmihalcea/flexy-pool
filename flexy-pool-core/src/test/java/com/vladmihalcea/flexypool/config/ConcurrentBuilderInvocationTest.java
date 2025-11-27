fa97d3e7c1531e14bb2e2b24cf24092e6f3b7a7e
Aleksandr Mikhailov <iam@fidonode.me>
2025-11-27 10:25:57 +0200

    Add problem reproducer as a separate test

---
+++ flexy-pool-core/src/test/java/com/vladmihalcea/flexypool/config/ConcurrentBuilderInvocationTest.java
@@ -0,0 +1,48 @@
+package com.vladmihalcea.flexypool.config;
+
+import com.vladmihalcea.flexypool.adaptor.PoolAdapterFactory;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.fail;
+
+public class ConcurrentBuilderInvocationTest {
+
+    @Test
+    public void testBuilder() throws InterruptedException, ExecutionException {
+        DataSource dataSource = Mockito.mock(DataSource.class);
+        PoolAdapterFactory<DataSource> poolAdapterFactory = Mockito.mock(PoolAdapterFactory.class);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        List<Future<FlexyPoolConfiguration<DataSource>>> results = executorService.invokeAll(List.of(
+                () -> createBuilder(dataSource, poolAdapterFactory),
+                () -> createBuilder(dataSource, poolAdapterFactory)
+        ));
+
+        for (Future<FlexyPoolConfiguration<DataSource>> result : results) {
+            try {
+                result.get();
+            } catch (NoSuchElementException e) {
+                fail("NoSuchElementException  was thrown");
+            } catch (IllegalStateException e) {
+                fail("IllegalStateException(" + e.getMessage() + ")  was thrown");
+            }
+        }
+
+
+    }
+
+    public FlexyPoolConfiguration<DataSource> createBuilder(DataSource dataSource, PoolAdapterFactory<DataSource> poolAdapterFactory) {
+        return new FlexyPoolConfiguration.Builder<DataSource>(
+                java.util.UUID.randomUUID().toString(), dataSource, poolAdapterFactory)
+                .build();
+    }
+}
